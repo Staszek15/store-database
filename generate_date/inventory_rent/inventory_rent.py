02fbdbfde08d06952da6c04f9b9991df41ba2af7
Julia Grzegorzewska <julek0309@gmail.com>
2023-06-26 22:51:51 +0200

    jestem super wika przyznaj to

--- generate_date/inventory_rent/inventory_rent.py
+++ generate_date/inventory_rent/inventory_rent.py
@@ -3,60 +3,6 @@ import random
 import numpy as np
 from scipy.special import softmax
 
-
-games_buy_prices = {
-    '1': 164.5,
-    '2': 65.00,
-    '3': 158.70,
-    '4': 170.32,
-    '5': 139.90,
-    '6': 119.19,
-    '7': 182.55,
-    '8': 179.90,
-    '9': 409.95,
-    '10': 269.90,
-    '11': 59.90,
-    '12': 169.90,
-    '13': 479.00,
-    '14': 169.00,
-    '15': 42.38,
-    '16': 100.00,
-    '17': 39.90,
-    '18': 62.50,
-    '19': 199.99,
-    '20': 112.89,
-    '21': 650.00,
-    '22': 219.00,
-    "23": 25.50,
-    '24': 188.65,
-    '25': 56.60,
-    '26': 350.00,
-    '27': 74.99,
-    '28': 129.99,
-    '29': 289.00,
-    '30': 277.80,
-    '31': 120.00,
-    '32': 115.60,
-    '33': 158.95,
-    '34': 28.00,
-    '35': 162.55,
-    '36': 185.2,
-    '37': 46.50,
-    '38': 239.90,
-    '39': 37.00,
-    '40': 103.00,
-    '41': 28.80,
-    '42': 120.00,
-    '43': 56.70,
-    '44': 79.90,
-    '45': 117.97,
-    '46': 25.60,
-    '47': 16.70,
-    '48': 180.00,
-    '49': 115.60,
-    '50': 133.20
-}
-games_rent_prices = {game_id: round(buy_price/15, 2) for game_id, buy_price in games_buy_prices.items()}
 games = pd.read_csv('game/game.csv')
 
 def exchange_players_to_game_id():
@@ -77,16 +23,12 @@ def exchange_players_to_game_id():
     return game_id
 
 
-def generate_inventory_rent():
+def generate_inventory_rent(prices):
     game_ids = exchange_players_to_game_id()
     random.shuffle(game_ids)
-    prices = [games_rent_prices[str(game_id)] for game_id in game_ids]
+    prices = [prices[str(game_id)] for game_id in game_ids]
     data = {'inventory_id': np.arange(1, 501),
                       'game_id': game_ids,
                       'price': prices,
-                      }
-    inventory_rent = pd.DataFrame(data)
-    return inventory_rent
-
-
-generate_inventory_rent()
+            }
+    return pd.DataFrame(data)
--- generate_date/main_execute.py
+++ generate_date/main_execute.py
@@ -1,13 +1,15 @@
 from staff.staff import *
 from inventory_rent.inventory_rent import *
 from address.address import *
+from const import *
+
 
 if __name__ == "__main__":
     generate_address().to_csv('address/address.csv')
     generate_staff().to_csv('staff/staff.csv')
     #create_staff_schedule
     #game
-    generate_inventory_rent().to_csv('inventory_rent/inventory_rent.csv')
+    generate_inventory_rent(games_rent_prices).to_csv('inventory_rent/inventory_rent.csv')
     #customer, rental
     #inventory_buy, purchase
     #tournament
