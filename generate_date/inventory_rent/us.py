ab7ddef5c4cb1929444a7e452aa40e8ef388e2e7
Julia Grzegorzewska <julek0309@gmail.com>
2023-06-27 09:38:30 +0200

    add available column to inventory_rent

--- generate_date/inventory_rent/us.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-import os
-
-cdir = os.getcwd()
-print("Current Directory: ", cdir)
-print("Parent Directory: ", os.path.dirname(cdir))
--- generate_date/main_execute.py
+++ generate_date/main_execute.py
@@ -9,8 +9,11 @@ if __name__ == "__main__":
     generate_staff().to_csv('staff/staff.csv')
     #create_staff_schedule
     #game
-    generate_inventory_rent(games_rent_prices).to_csv('inventory_rent/inventory_rent.csv')
+    generate_inventory_rent(games_rent_prices, pd.read_csv('game/game.csv')).to_csv('inventory_rent/inventory_rent.csv')
     #customer, rental
+    inv_rent = pd.read_csv('inventory_rent/inventory_rent.csv')
+    rental = pd.read_csv('customer_rental/rental.csv')
+    check_if_rent_available(inv_rent, rental).to_csv('inventory_rent/inventory_rent.csv')
     #inventory_buy, purchase
     #tournament
     #tournament_results
