62fa5bac2dbd32c6b2fc4a43a83fac14084a440d
Mateusz Stasiak <mateusz.stasiak15@wp.pl>
2023-06-29 13:28:17 +0200

    naprawione relacje

--- generate_data/inserts.py
+++ generate_data/inserts.py
@@ -14,38 +14,43 @@ def insert(df_games, df_addresses, df_customers, df_inventory_buy, df_inventory_
 
     engine = create_engine(url_object)
     conn = engine.connect()
+    conn.execute(text("SET FOREIGN_KEY_CHECKS=0"))
+    
 
-    conn.execute(text('TRUNCATE TABLE games'))
+
+    #conn.execute(text('TRUNCATE TABLE games'))
     df_games.to_sql("games", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE addresses'))
+    #conn.execute(text('TRUNCATE TABLE addresses'))
     df_addresses.to_sql("addresses", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE customers'))
+    #conn.execute(text('TRUNCATE TABLE customers'))
     df_customers.to_sql("customers", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE inventory_buy'))
+    #conn.execute(text('TRUNCATE TABLE inventory_buy'))
     df_inventory_buy.to_sql("inventory_buy", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE inventory_rent'))
+    #conn.execute(text('TRUNCATE TABLE inventory_rent'))
     df_inventory_rent.to_sql("inventory_rent", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE purchases'))
+    #conn.execute(text('TRUNCATE TABLE purchases'))
     df_purchases.to_sql("purchases", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE rentals'))
+    #conn.execute(text('TRUNCATE TABLE rentals'))
     df_rentals.to_sql("rentals", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE staff'))
+    #conn.execute(text('TRUNCATE TABLE staff'))
     df_staff.to_sql("staff", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE tournament'))
+    #conn.execute(text('TRUNCATE TABLE tournament'))
     df_tournament.to_sql("tournament", con=engine, if_exists="replace", index=False)
 
-    conn.execute(text('TRUNCATE TABLE tournament_results'))
+    #conn.execute(text('TRUNCATE TABLE tournament_results'))
     df_tournament_results.to_sql("tournament_results", con=engine, if_exists="replace", index=False)
 
     
+    conn.execute(text("SET FOREIGN_KEY_CHECKS=1"))
+
 
 
 
