0e6f6c4dd13c4d77320c9c21f1c30f35431d269f
Mateusz Stasiak <mateusz.stasiak15@wp.pl>
2023-06-30 14:48:59 +0200

    dziala dodawanie kluczy do tabel

--- generate_data/connection.py
+++ generate_data/connection.py
@@ -6,20 +6,6 @@ def connect():
 
     print("Connecting to database")
 
-    df_games = pd.read_csv('game/game.csv')
-    df_inventory_rent = pd.read_csv('inventory_rent/inventory_rent.csv')
-    df_inventory_buy = pd.read_csv('inventory_buy_purchase/inventory_buy.csv')
-    df_addresses = pd.read_csv('address/address.csv')
-    df_customers = pd.read_csv('customer_rental/customers.csv')
-    df_purchases = pd.read_csv('inventory_buy_purchase/purchases.csv')
-    df_rentals = pd.read_csv('customer_rental/rentals.csv')
-    df_staff = pd.read_csv('staff/staff.csv')
-    df_tournament = pd.read_csv('tournament/tournaments.csv')
-    df_tournament_results = pd.read_csv('tournament_results/tournament_results.csv')
-
-    print("csv loaded")
-
-
     url_object = URL.create(
         "mysql+pymysql",
         username="team21",
@@ -31,12 +17,12 @@ def connect():
     engine = create_engine(url_object)
     conn = engine.connect()
     print("connected")
-    #conn.execute(text("SET FOREIGN_KEY_CHECKS=0"))
+    conn.execute(text("SET FOREIGN_KEY_CHECKS=0"))
     conn.execute(text("DROP TABLE IF EXISTS addresses, customers, games, inventory_buy, inventory_rent, purchases, rentals, staff, tournament, tournament_results"))
     
     print("tables dropped")
 
-    fd = open('create_tables.sql', 'r')
+    fd = open('chat_sql.sql', 'r')
     sqlTablesFile = fd.read()
     fd.close()
     sqlTablesCode = sqlTablesFile.split(';')[:-1]   # without last one because last query also ends with ';' so last element is empty
@@ -46,7 +32,20 @@ def connect():
         conn.execute(text(command))    
 
 
-    #conn.execute(text('TRUNCATE TABLE games'))
+    
+    df_games = pd.read_csv('game/game.csv')
+    df_inventory_rent = pd.read_csv('inventory_rent/inventory_rent.csv')
+    df_inventory_buy = pd.read_csv('inventory_buy_purchase/inventory_buy.csv')
+    df_addresses = pd.read_csv('address/address.csv')
+    df_customers = pd.read_csv('customer_rental/customers.csv')
+    df_purchases = pd.read_csv('inventory_buy_purchase/purchases.csv')
+    df_rentals = pd.read_csv('customer_rental/rentals.csv')
+    df_staff = pd.read_csv('staff/staff.csv')
+    df_tournament = pd.read_csv('tournament/tournaments.csv')
+    df_tournament_results = pd.read_csv('tournament_results/tournament_results.csv')
+
+
+    """#conn.execute(text('TRUNCATE TABLE games'))
     df_games.to_sql("games", con=engine, if_exists="replace", index=False, )
 
     #conn.execute(text('TRUNCATE TABLE addresses'))
@@ -74,24 +73,10 @@ def connect():
     df_tournament.to_sql("tournament", con=engine, if_exists="replace", index=False)
 
     #conn.execute(text('TRUNCATE TABLE tournament_results'))
-    df_tournament_results.to_sql("tournament_results", con=engine, if_exists="replace", index=False)
-
-
-
-    print("tables dropped")
-
-    """fm = open('foreign_keys.sql', 'r')
-    sqlKeysFile = fm.read()
-    fm.close()
-    sqlKeysCode = sqlKeysFile.split(';')[:-1]   # without last one because last query also ends with ';' so last element is empty
-
-    for command in sqlKeysCode:
-        print(text(command))
-        conn.execute(text(command))""" 
-
+    df_tournament_results.to_sql("tournament_results", con=engine, if_exists="replace", index=False)"""
     
 
-    #conn.execute(text("SET FOREIGN_KEY_CHECKS=1"))
+    conn.execute(text("SET FOREIGN_KEY_CHECKS=1"))
     conn.close()
 
 
