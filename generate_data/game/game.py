b2aa1eb05694349201b65600c9c63cfc98b2628d
Karolina Wypych <wypych.kar.0101@gmail.com>
2023-06-30 20:45:39 +0200

    comments to game

--- generate_data/game/game.py
+++ generate_data/game/game.py
@@ -2,6 +2,11 @@ import pandas as pd
 
 
 def suitable_for_tournament(dataframe):
+    """
+    Function that cheks if a game is suitable for a tournament.
+    :param dataframe: dataframe containing information on games
+    It returns True if game is ok for a tournament and False if not.
+    """
     if dataframe['duration'] <= 120 and dataframe['max_players'] <= 40:
         return True
     else:
@@ -9,6 +14,11 @@ def suitable_for_tournament(dataframe):
 
 
 def generate_game():
+    """
+    Function that takes an dataset downloaded from the Internet and add information
+    about minimum and maximum number of players in a team. 
+    It returns a DataFrame which later will be used to fill a game table in a database. 
+    """
     games = pd.read_csv('game/games_dataset.csv', index_col = 0)
     games.columns.str.lower()   
     games = games.rename(columns={'age':'min_age'})
