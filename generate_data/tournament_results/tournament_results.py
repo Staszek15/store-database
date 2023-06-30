971f9cd798f88e23296f835b1d498bf033eb808a
Karolina Wypych <wypych.kar.0101@gmail.com>
2023-06-30 22:45:40 +0200

    documentation to files py

--- generate_data/tournament_results/tournament_results.py
+++ generate_data/tournament_results/tournament_results.py
@@ -4,10 +4,17 @@ import random
 
 
 def generate_tournament_results(tournaments, customers, games):
+    """
+    Function that choose customers who take part in a tournament, assigns points and ranks.
+    :param tournaments: table with tournaments
+    :customers: table with customers
+    :games: table with games
+    """
     participants_list = []
     score_list = []
     tour_id_list = []
     place_list = []
+    #takes into consideration only this tournaments that have already taken place
     tournaments = tournaments[tournaments['staff_id'].notnull()]
     
     for i in range(len(tournaments)):
@@ -15,7 +22,7 @@ def generate_tournament_results(tournaments, customers, games):
         games_row = games[games['game_id'] == tour_row.game_id]
 
         tour_id = [tour_row.tournament_id for i in range(int(tour_row.total_players_number))]
-        customers_id = random.sample(sorted(customers.customer_id),int(tour_row.total_players_number))
+        customers_id = random.sample(sorted(customers.customer_id),tour_row.total_players_number)
 
         tour_id_list.extend(tour_id)
         participants_list.extend(customers_id)
@@ -23,7 +30,9 @@ def generate_tournament_results(tournaments, customers, games):
         teams_number = int(np.floor(tour_row.total_players_number / tour_row.team_players_number))        
 
         if tour_row.team_players_number > 1:
+            # every member of a team takes the same place
             place = sorted(list(np.arange(1, teams_number + 1)) * int(games_row.max_players_in_team.iloc[0]))
+            #score from 1 (last place) to number of teams (first place)
             score = place[::-1]
             score_list.extend(score)
             place_list.extend(place)
